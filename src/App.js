83319daa9fe98d61c4b9b10611a0435e63c90a74
Kieron Wilkinson <kieron.wilkinson@insightinvestment.com>
2018-07-29 07:58:04 +0100

    Moved data fetch to in-memory and TeamCity source classes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import Collections from './Collections';
 import Versions from './Versions';
 import './App.css'
+import InMemorySource from "./inmemory/InMemorySource"
+import TeamCitySource from "./teamcity/TeamCitySource"
 
 class App extends React.Component {
   constructor() {
@@ -35,8 +37,10 @@ class App extends React.Component {
   }
   
   load() {
-    let promise = fetchReleasesPerApp(fetchPlanetDeploys, data => data)
-    //let promise = fetchReleasesPerApp(fetchTeamCityDeploys, convertTeamCityFormat)
+    let source = new InMemorySource()
+    // let source = new TeamCitySource()
+    let promise = fetchReleasesPerApp(source.fetch, data => data)
+    // let promise = fetchReleasesPerApp(source, convertTeamCityFormat)
     
     promise.then(releasesPerApp => {
       let filteredReleasesPerApp = filterApps(this.state.filter, releasesPerApp)
@@ -254,30 +258,6 @@ function statusIconClass(status) {
   }
 }
 
-
-function fetchTeamCityDeploys() {
-  let url = "http://localhost:8080/guestAuth/app/rest/latest/buildTypes?locator=type:deployment&fields=buildType(id,name,type,builds($locator(canceled:false,count:1),build(number,status,finishDate,properties(property(name,value)))))"
-  return fetch(url, {
-      headers: {
-//        'Authorization': 'Basic '+btoa('admin:'),
-//        'Access-Control-Allow-Credentials': true,
-//        'Credentials': 'include',
-        'Accept': 'application/json',
-//        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error('Could not fetch from URL')
-      }
-    })
-//    .then(data => console.log(data))
-//    .catch(error => console.log(error))
-}
-
-
 function assertEqual(actual, expected) {
   let equal = JSON.stringify(actual) === JSON.stringify(expected)
   if (equal) {
@@ -287,163 +267,5 @@ function assertEqual(actual, expected) {
   }
 }
 
-
-function fetchPlanetDeploys() {
-  return new Promise((resolve, reject) => {
-    resolve(fetchPlanetDeploysSync())
-  })
-}
-
-function fetchPlanetDeploysSync() {
-  return [
-    {
-      name: "Mercury",
-      version: "1.3.3",
-      environment: "DEV",
-      time: "2018-06-09T21:58:12",
-      status: "SUCCESS"
-    },
-    {
-      name: "Mercury",
-      version: "1.3.3",
-      environment: "TST",
-      time: "2018-06-09T21:58:12",
-      status: "SUCCESS"
-    },
-    {
-      name: "Mercury",
-      version: "1.3.3",
-      environment: "UAT",
-      time: "2018-06-09T21:58:12",
-      status: "SUCCESS"
-    },
-    {
-      name: "Mercury",
-      version: "1.3.3",
-      environment: "PRD",
-      time: "2018-06-09T21:58:12",
-      status: "SUCCESS"
-    },
-    {
-      name: "Venus",
-      version: "17.0.1",
-      environment: "PRD",
-      time: "2018-06-04T22:20:33",
-      status: "SUCCESS"
-    },
-    {
-      name: "Venus",
-      version: "17.0.2",
-      environment: "TST",
-      time: new Date().toISOString(),
-      status: "RUNNING"
-    },
-    {
-      name: "Venus",
-      version: "17.0.1",
-      environment: "UAT",
-      time: "2018-06-01T22:20:33",
-      status: "SUCCESS"
-    },
-    {
-      name: "Venus",
-      version: "17.0.2",
-      environment: "DEV",
-      time: "2018-06-01T22:20:33",
-      status: "SUCCESS"
-    },
-    {
-      name: "Earth",
-      version: "5.3.0",
-      environment: "DEV",
-      time: "2018-06-04T21:59:51",
-      status: "FAILURE"
-    },
-    {
-      name: "Earth",
-      version: "5.2.1",
-      environment: "TST",
-      time: "2018-06-04T21:59:51",
-      status: "SUCCESS"
-    },
-    {
-      name: "Earth",
-      version: "5.2.1",
-      environment: "UAT",
-      time: "2018-06-04T21:59:51",
-      status: "SUCCESS"
-    },
-    {
-      name: "Earth",
-      version: "5.2.1",
-      environment: "PRD",
-      time: "2018-06-04T21:59:51",
-      status: "SUCCESS"
-    },
-    {
-      name: "Uranus",
-      version: "11.2.113",
-      environment: "DEV",
-      time: "2018-06-04T22:20:33",
-      status: "SUCCESS"
-    },
-    {
-      name: "Uranus",
-      version: "11.2.113",
-      environment: "TST",
-      time: "2018-06-04T22:20:33",
-      status: "SUCCESS"
-    },
-    {
-      name: "Uranus",
-      version: "11.2.112",
-      environment: "PRD",
-      time: "2018-06-04T22:20:33",
-      status: "OTHER"
-    },
-    {
-      name: "Ceres",
-      version: "1.0.0",
-      environment: "DEV",
-      time: "2018-06-16T09:54:00",
-      status: "SUCCESS"
-    },
-    {
-      name: "Ceres",
-      version: "1.0.0",
-      environment: "TST",
-      time: new Date().toISOString(),
-      status: "RUNNING"
-    },
-    ...randomDeployment("Mars"),
-    ...randomDeployment("Jupiter"),
-    ...randomDeployment("Saturn"),
-    ...randomDeployment("Neptune"),
-    ...randomDeployment("Pluto"),
-    ...randomDeployment("Haumea"),
-    ...randomDeployment("Makemake"),
-    ...randomDeployment("Eris"),
-    ...randomDeployment("Quaoar"),
-    ...randomDeployment("Sedna"),
-    ...randomDeployment("Orcas")
-  ]
-}
-
-function randomDeployment(name) {
-  let envs = ["DEV", "TST", "UAT", "PRD"]
-  let ver = (max) => Math.floor(Math.random() * max)
-  let major = ver(9) + 1
-  let minor = ver(20)
-  let bugfix = [ver(3), ver(3), ver(3), ver(3)].sort().reverse()
-  let failure = () => ver(15) < 2
-  
-  return [0, 1, 2, 3].map(i => ({
-    name: name,
-    version: major + "." + minor + "." + bugfix[i],
-    environment: envs[i],
-    time: "2018-06-16T09:54:00",
-    status: failure() ? "FAILURE" : "SUCCESS"
-  }))
-}
 
 export default App;
---
+++ src/teamcity/TeamCitySource.js
@@ -0,0 +1,27 @@
+class TeamCitySource {
+
+  fetch() {
+    let url = "http://localhost:8080/guestAuth/app/rest/latest/buildTypes?locator=type:deployment&fields=buildType(id,name,type,builds($locator(canceled:false,count:1),build(number,status,finishDate,properties(property(name,value)))))"
+    return fetch(url, {
+      headers: {
+//        'Authorization': 'Basic '+btoa('admin:'),
+//        'Access-Control-Allow-Credentials': true,
+//        'Credentials': 'include',
+        'Accept': 'application/json',
+//        'Content-Type': 'application/json'
+      }
+    })
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Could not fetch from URL')
+          }
+        })
+//    .then(data => console.log(data))
+//    .catch(error => console.log(error))
+  }
+
+}
+
+export default TeamCitySource

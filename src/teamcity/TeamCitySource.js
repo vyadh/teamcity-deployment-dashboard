975eed5e8a544746b22d67ee4f59754d892bc67a
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2018-12-02 11:13:01 +0000

    Fixed CORS issue invoking TeamCity

--- src/teamcity/TeamCitySource.js
+++ src/teamcity/TeamCitySource.js
@@ -1,5 +1,9 @@
 import TeamCityConverter from "./TeamCityConverter"
 
+/*
+ * Requires setting TeamCity internal CORS property 'rest.cors.origins'. For example:
+ *   rest.cors.origins=http://localhost:3000,http://127.0.0.1:3000
+ */
 class TeamCitySource {
 
   static converter(data) {
@@ -8,16 +12,19 @@ class TeamCitySource {
   }
 
   fetch() {
-    let url = "http://localhost:8080/guestAuth/app/rest/latest/buildTypes?locator=type:deployment&fields=buildType(id,name,type,builds($locator(canceled:false,count:1),build(number,status,finishDate,properties(property(name,value)))))"
+    console.log("Fetching deployments from TeamCity...")
+
+    let query = "locator=type:deployment&fields=buildType(id,name,type,builds($locator(canceled:false,count:1),build(number,status,finishDate,properties(property(name,value)))))"
+    // let url = "http://localhost:8080/guestAuth/app/rest/latest/buildTypes?" + query
+    // let url = "http://localhost:8080/httpAuth/app/rest/latest/buildTypes?" + query
+    let url = "http://localhost:8080/app/rest/latest/buildTypes?" + query
+
     return fetch(url, {
-      headers: {
-//        'Authorization': 'Basic '+btoa('admin:'),
-//        'Access-Control-Allow-Credentials': true,
-//        'Credentials': 'include',
-        'Accept': 'application/json',
-//        'Content-Type': 'application/json'
-      }
-    })
+          credentials: 'include',
+          headers: {
+            'Accept': 'application/json'
+          }
+        })
         .then(response => {
           if (response.ok) {
             return response.json()
@@ -25,8 +32,8 @@ class TeamCitySource {
             throw new Error('Could not fetch from URL')
           }
         })
-//    .then(data => console.log(data))
-//    .catch(error => console.log(error))
+        // .then(data => { console.log(data); return data })
+        .catch(error => console.log(error))
   }
 
 }

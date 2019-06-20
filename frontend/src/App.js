acb8472af82463e0d440c8fe9baa232a1c5c9e03
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2019-06-20 10:26:15 +0100

    Extracted Deployment component to its own files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
-import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
-import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import { Search } from './components/Search'
-import * as dateTimes from './util/dateTimes'
 import * as deploysProcessor from './deploysProcessor'
 import './App.css'
 import {filterApps} from "./searchApps"
+import {Deployment} from "./Deployment";
 
 const App = ({configuration}) => {
 
@@ -99,58 +94,9 @@ const DeployRow = ({app, environments, deploys}) => (
     <tr>
       <td>{app}</td>
       {environments.map(env =>
-          <Deploy key={env} environment={env} deploys={deploys}/>)}
+          <Deployment key={env} environment={env} deploys={deploys}/>)}
     </tr>
 )
-
-const Deploy = ({environment, deploys}) => {
-  let found = deploys.find(deploy => deploy.environment === environment)
-
-  if (found === undefined) {
-    return <td key={environment}/>
-  } else {
-    return <td key={environment}><Build deploy={found}/></td>
-  }
-}
-
-const Build = ({deploy}) => (
-    <a href={deploy.link}>
-      <div className="build">
-        <div className={`build-status ${statusClass(deploy.status)}`}>
-          <StatusIcon status={deploy.status} latest={deploy.latest}/>
-        </div>
-        <div className="build-info grow">
-          <span className="build-version">{deploy.version}</span>
-          <span className="build-time">{dateTimes.format(deploy.time)}</span>
-        </div>
-      </div>
-    </a>
-)
-
-const StatusIcon = ({status, latest}) => {
-  let iconType = statusIconClass(status)
-  let rotateClass = status === "RUNNING" ? "fa-spin" : ""
-  let ageClass = latest ? "status-latest" : "status-older"
-  let classes = `status-icon ${statusClass(status)} ${rotateClass} ${ageClass}`
-
-  return <FontAwesomeIcon icon={iconType} className={classes}/>
-}
-
-const statusClass = (status) => {
-  return `status-${status}`
-}
-
-const statusIconClass = status => {
-  if (status === "SUCCESS") {
-    return faCheckCircle
-  } else if (status === "FAILURE") {
-    return faExclamationCircle
-  } else if (status === "RUNNING") {
-    return faCircleNotch // fa-spin";
-  } else {
-    return faQuestionCircle
-  }
-}
 
 
 export default App;
---
+++ frontend/src/Deployment.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import * as dateTimes from "./util/dateTimes";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCheckCircle, faCircleNotch, faExclamationCircle, faQuestionCircle} from "@fortawesome/free-solid-svg-icons";
+import './Deployment.css'
+
+export const Deployment = ({environment, deploys}) => {
+    let found = deploys.find(deploy => deploy.environment === environment)
+
+    if (found === undefined) {
+        return <td key={environment}/>
+    } else {
+        return <td key={environment}><Build deploy={found}/></td>
+    }
+}
+
+const Build = ({deploy}) => (
+    <a href={deploy.link}>
+        <div className="build">
+            <div className={`build-status ${statusClass(deploy.status)}`}>
+                <StatusIcon status={deploy.status} latest={deploy.latest}/>
+            </div>
+            <div className="build-info grow">
+                <span className="build-version">{deploy.version}</span>
+                <span className="build-time">{dateTimes.format(deploy.time)}</span>
+            </div>
+        </div>
+    </a>
+)
+
+const StatusIcon = ({status, latest}) => {
+    let iconType = statusIconClass(status)
+    let rotateClass = status === "RUNNING" ? "fa-spin" : ""
+    let ageClass = latest ? "status-latest" : "status-older"
+    let classes = `status-icon ${statusClass(status)} ${rotateClass} ${ageClass}`
+
+    return <FontAwesomeIcon icon={iconType} className={classes}/>
+}
+
+const statusClass = (status) => {
+    return `status-${status}`
+}
+
+const statusIconClass = status => {
+    if (status === "SUCCESS") {
+        return faCheckCircle
+    } else if (status === "FAILURE") {
+        return faExclamationCircle
+    } else if (status === "RUNNING") {
+        return faCircleNotch // fa-spin";
+    } else {
+        return faQuestionCircle
+    }
+}

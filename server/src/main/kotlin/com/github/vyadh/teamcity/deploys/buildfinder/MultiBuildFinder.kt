6202cbade1bbcc839d0ac5d8e4595401f2f4a9c0
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2019-07-06 11:12:09 +0100

    #6 Only read enough build history to satisfy expected environments (and also give up after 1000)

--- server/src/main/kotlin/com/github/vyadh/teamcity/deploys/buildfinder/MultiBuildFinder.kt
+++ server/src/main/kotlin/com/github/vyadh/teamcity/deploys/buildfinder/MultiBuildFinder.kt
@@ -1,42 +1,46 @@
 package com.github.vyadh.teamcity.deploys.buildfinder
 
-import jetbrains.buildServer.serverSide.BuildHistory
-import jetbrains.buildServer.serverSide.SBuild
-import jetbrains.buildServer.serverSide.SBuildType
-import jetbrains.buildServer.serverSide.SFinishedBuild
+import com.github.vyadh.teamcity.deploys.processing.DeployExtractor.environmentName
+import jetbrains.buildServer.serverSide.*
 import jetbrains.buildServer.util.ItemProcessor
+import java.util.*
 import java.util.stream.Stream
+import kotlin.collections.ArrayList
 
 /**
  * Walks the TeamCity build history trying to find all builds for the configured
  * environments. This is necessary for situations where a single configuration
- * deploys to multiple environments. This is potentially rather slow, especially
- * if not all environments are not found as it will search the entire history.
+ * deploys to multiple environments. This is potentially rather slow.
+ *
+ * Unlike [LastBuildFinder] which leaves filtering to the client, this class filters
+ * to the known environments as a best-effort way of avoiding reading the entire
+ * build history once it has found the ones expected.
  *
  * Note that we're not concerned with duplicates as that is handled later in the
- * pipe by DeployDuplicateResolver.
+ * pipe by [com.github.vyadh.teamcity.deploys.processing.DeployDuplicateResolver].
  */
 class MultiBuildFinder(
       private val history: BuildHistory,
+      private val environmentKey: String,
       private val environments: List<String>
 ) : BuildFinder {
 
-  /** Point at which to give up searching to problems with massive build histories */
-  val upperBound = 1000
+  companion object {
+    /** Point at which to give up searching to problems with massive build histories */
+    const val upperBound = 1000
+  }
 
   override fun find(type: SBuildType): Stream<SBuild> {
     return Stream.concat(running(type), finished(type))
   }
 
-  private fun running(type: SBuildType) = type.runningBuilds.stream()
+  private fun running(type: SBuildType): Stream<SRunningBuild> {
+    return type.runningBuilds.stream()
+          .filter { environments.contains(environmentName(it, environmentKey)) }
+  }
 
   private fun finished(type: SBuildType): Stream<SFinishedBuild> {
-    val builds = ArrayList<SFinishedBuild>(100)
-
-    val captureMostRecent = ItemProcessor { item: SFinishedBuild ->
-      builds.add(item)
-      true
-    }
+    val processor = MatchingItemProcessor(environmentKey, environments)
 
     history.processEntries(
           type.internalId,
@@ -44,9 +48,32 @@ class MultiBuildFinder(
           true,
           false,
           false,
-          captureMostRecent
+          processor
     )
-    return builds.stream()
+
+    return processor.builds.stream()
+  }
+
+  class MatchingItemProcessor(val environmentKey: String, environments: List<String>) :
+        ItemProcessor<SFinishedBuild> {
+
+    val builds = ArrayList<SFinishedBuild>(100)
+
+    private val leftToMatch = LinkedList(environments)
+    private var countDown = upperBound
+
+    override fun processItem(build: SFinishedBuild): Boolean {
+      val env = environmentName(build, environmentKey)
+
+      if (leftToMatch.contains(env)) {
+        builds.add(build)
+        leftToMatch.remove(env)
+      }
+
+      countDown--
+      return leftToMatch.isNotEmpty() && countDown > 0
+    }
+
   }
 
 }
--- server/src/test/kotlin/com/github/vyadh/teamcity/deploys/buildfinder/MultiBuildFinderTest.kt
+++ server/src/test/kotlin/com/github/vyadh/teamcity/deploys/buildfinder/MultiBuildFinderTest.kt
@@ -5,9 +5,11 @@ import com.github.vyadh.teamcity.deploys.processing.BuildMocks.deploymentBuildTy
 import com.github.vyadh.teamcity.deploys.processing.BuildMocks.finished
 import com.github.vyadh.teamcity.deploys.processing.BuildMocks.running
 import jetbrains.buildServer.serverSide.SBuild
+import jetbrains.buildServer.serverSide.SFinishedBuild
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.util.stream.Collectors.toList
+import java.util.stream.Stream
 
 internal class MultiBuildFinderTest {
 
@@ -18,7 +20,7 @@ internal class MultiBuildFinderTest {
   @Test
   internal fun noBuilds() {
     val type = deploymentBuildType()
-    val finder = MultiBuildFinder(SimulatedBuildHistory.empty(), emptyList())
+    val finder = MultiBuildFinder(SimulatedBuildHistory.empty(), envKey, emptyList())
 
     val result = finder.find(type).collect(toList())
 
@@ -31,7 +33,8 @@ internal class MultiBuildFinderTest {
           running(properties("Vega", "DEV")),
           running(properties("Vega", "PRD"))
     ))
-    val finder = MultiBuildFinder(SimulatedBuildHistory.empty(), listOf("DEV", "PRD"))
+    val finder = MultiBuildFinder(
+          SimulatedBuildHistory.empty(), envKey, listOf("DEV", "PRD"))
 
     val result = finder.find(type).map { info(it) }.collect(toList())
     
@@ -44,7 +47,7 @@ internal class MultiBuildFinderTest {
     val finder = MultiBuildFinder(SimulatedBuildHistory(
           finished(properties("Vega", "DEV")),
           finished(properties("Vega", "PRD"))
-    ), listOf("DEV", "PRD"))
+    ), envKey, listOf("DEV", "PRD"))
 
     val result = finder.find(type).map { info(it) }.collect(toList())
 
@@ -61,8 +64,9 @@ internal class MultiBuildFinderTest {
           finished(properties("Vega", "UAT")),
           finished(properties("Vega", "PRD"))
     )
+    val environments = listOf("DEV", "TST", "UAT", "PRD")
     val type = buildTypeWith(running)
-    val finder = MultiBuildFinder(finished, listOf("DEV", "TST", "UAT", "PRD"))
+    val finder = MultiBuildFinder(finished, envKey, environments)
 
     val result = finder.find(type).map { info(it) }.collect(toList())
 
@@ -78,14 +82,15 @@ internal class MultiBuildFinderTest {
   internal fun onlyBuildsForSpecifiedEnvironments() {
     val running = listOf(
           running(properties("Sol", "DEV")),
-          running(properties("Sol", "Unknown"))
+          running(properties("Sol", "UnknownRunning"))
     )
     val finished = SimulatedBuildHistory(
           finished(properties("Sol", "UAT")),
-          finished(properties("Sol", "Unknown"))
+          finished(properties("Sol", "UnknownFinished"))
     )
+    val environments = listOf("DEV", "UAT")
     val type = buildTypeWith(running)
-    val finder = MultiBuildFinder(finished, listOf("DEV", "UAT"))
+    val finder = MultiBuildFinder(finished, envKey, environments)
 
     val result = finder.find(type).map { info(it) }.collect(toList())
 
@@ -95,17 +100,68 @@ internal class MultiBuildFinderTest {
     )
   }
 
-  
-  private fun properties(project: String, env: String): Map<String, String> {
+  @Test
+  internal fun matchOnlyOneEnvironmentFromHistory() {
+    val finished = SimulatedBuildHistory(
+          finished(properties("Sol", "DEV", "1.0")),
+          finished(properties("Sol", "UAT", "1.1")),
+          finished(properties("Sol", "DEV", "2.0")),
+          finished(properties("Sol", "UAT", "2.1"))
+    )
+    val environments = listOf("DEV", "UAT")
+    val type = deploymentBuildType()
+    val finder = MultiBuildFinder(finished, envKey, environments)
+
+    val result = finder.find(type).map {
+      listOf(project(it), env(it), version(it))
+    }.collect(toList())
+
+    assertThat(result).containsExactly(
+          listOf("Sol", "DEV", "1.0"),
+          listOf("Sol", "UAT", "1.1")
+    )
+  }
+
+  @Test
+  internal fun doesNotBotherWithVeryOldHistory() {
+    val buildsDev = Stream.iterate(1, { it + 1} )
+          .map { i -> finished(properties("Vega", "DEV", i.toString()))}
+          .limit(1000)
+    val buildsUat = Stream.of(finished(properties("Vega", "UAT", "1rc")))
+    val history = SimulatedBuildHistory(*toArray(Stream.concat(buildsDev, buildsUat)))
+    val environments = listOf("DEV", "UAT")
+    val type = deploymentBuildType()
+    val finder = MultiBuildFinder(history, envKey, environments)
+
+    val result = finder.find(type).map {
+      listOf(project(it), env(it), version(it))
+    }.collect(toList())
+
+    assertThat(result).containsExactly(
+          listOf("Vega", "DEV", "1")
+    )
+  }
+
+  private fun properties(project: String, env: String, version: String = "1.0"):
+        Map<String, String> {
+
     return mapOf(
           Pair(projectKey, project),
-          Pair(versionKey, "1.0"),
+          Pair(versionKey, version),
           Pair(envKey, env)
     )
   }
 
   private fun info(build: SBuild): Pair<String, String> {
-    return Pair(build.buildOwnParameters[projectKey]!!, build.buildOwnParameters[envKey]!!)
+    return Pair(project(build), env(build))
+  }
+
+  private fun project(build: SBuild) = build.buildOwnParameters[projectKey]!!
+  private fun env(build: SBuild) = build.buildOwnParameters[envKey]!!
+  private fun version(build: SBuild) = build.buildOwnParameters[versionKey]!!
+
+  private fun toArray(stream: Stream<SFinishedBuild>): Array<SFinishedBuild> {
+    return stream.toArray { size -> arrayOfNulls<SFinishedBuild>(size) }
   }
 
 }

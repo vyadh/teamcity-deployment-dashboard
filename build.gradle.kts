389e48b806ddae2dcd056c12655d661b1d3d4883
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2019-07-14 23:22:52 +0100

    Bump version to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 group = "com.github.vyadh.teamcity"
-version = "1.7.0${buildNumber()}"
+version = "1.8.0${buildNumber()}"
 
 extra["teamcityVersion"] = findProperty("teamcity.version") ?: "2018.2"
 

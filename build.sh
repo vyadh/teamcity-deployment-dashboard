0af092c55cde6a9e314315dbcaa726c320b039d5
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2020-07-12 09:07:31 +0100

    #23 Clarify where plugin should be copied to redeploy

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ DOCKER_BUILDKIT=1 docker build -t deployment-dashboard .
 id=$(docker create deployment-dashboard)
 docker cp $id:/home/gradle/deployment-dashboard.zip deployment-dashboard.zip
 docker rm -v $id
+
+echo "Done. Copy 'deployment-dashboard.zip' to the '<TeamCityData>/plugins/' folder and reload from the plugins page"

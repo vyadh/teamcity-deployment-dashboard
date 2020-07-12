41af7e5abfed805074d8dec915f6a0136d37ca0a
Kieron Wilkinson <kieron.wilkinson@gmail.com>
2020-07-12 08:32:54 +0100

    #23 Fixed tests to run in container build

--- frontend/src/util/dateTimes.test.js
+++ frontend/src/util/dateTimes.test.js
@@ -3,7 +3,7 @@ import {format, isToday} from "./dateTimes"
 const dateFormatter = new Intl.DateTimeFormat(
   'en-GB', { day: "numeric", month: "long", year: "numeric" })
 const timeFormatter = new Intl.DateTimeFormat(
-  'en-GB', { hour: 'numeric', minute: 'numeric', second: 'numeric' })
+  'en-GB', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
 
 it('time is today', () => {
   expect(isToday(new Date())).toBe(true)
@@ -14,19 +14,18 @@ it('time is not today', () => {
 })
 
 it('date-time is formatted to short string', () => {
-  // Locale is US in tests
-  expect(format('2019-06-28T08:30:00', dateFormatter)).toBe('June 28, 2019')
+  expect(format('2019-06-28T08:30:00', dateFormatter)).toBe('28 June 2019')
 })
 
 it('date-time is formatted as time', () => {
   let date = new Date().toISOString().substring(0, 10)
 
-  expect(format(date + 'T09:45:22', timeFormatter)).toBe('9:45:22 AM')
+  expect(format(date + 'T09:45:22', timeFormatter)).toBe('9:45:22 am')
 })
 
 it('time is formatted to the correct time zone', () => {
   let date = new Date().toISOString().substring(0, 10)
   let timeToday = date + 'T18:30:00Z' // As from server in UTC
 
-  expect(format(timeToday, timeFormatter)).toBe('7:30:00 PM') // as BST
+  expect(format(timeToday, timeFormatter)).toBe('7:30:00 pm') // as BST
 })
